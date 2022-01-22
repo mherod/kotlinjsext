74006fd247c0464a7dd486c426050800760f13a4
Matthew Herod <matthew.herod@myunidays.com>
2022-01-22 16:40:45 +0000

    Create LazyTests.js

---
+++ __tests__/LazyTests.js
@@ -0,0 +1,18 @@
+const {lazy} = require("kotlinjsext")
+
+test('lazy is lazy', () => {
+
+    let count = 0;
+
+    let expected = "hello";
+
+    const lazyProvider = lazy(() => {
+        count++;
+        return expected;
+    })
+
+    for (let i = 0; i < 100; i++) {
+        expect(lazyProvider.value).toEqual(expected);
+    }
+    expect(count).toBe(1);
+});
