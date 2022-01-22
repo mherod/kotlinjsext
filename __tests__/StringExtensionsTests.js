7eddc354245b8fb0265b5a4c86cc908334fcb031
Matthew Herod <matthew.herod@myunidays.com>
2022-01-22 16:36:12 +0000

    Setup tests

---
+++ __tests__/StringExtensionsTests.js
@@ -0,0 +1,12 @@
+require("kotlinjsext")
+
+test('distinctBy does what you think', () => {
+    const input = ["hello", "hello", "world"]
+
+    const result = input.distinctBy(a => a)
+
+    let expected = ["hello", "world"];
+
+    expect(result.length).toBe(expected.length);
+    expect(result).toEqual(expected);
+});
