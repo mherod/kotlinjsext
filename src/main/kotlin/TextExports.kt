a0d6c11b1963b9fd3aaa4a254efdeaa086d31e59
Matthew Herod <matthew.herod@myunidays.com>
2022-01-23 03:21:12 +0000

    Update TextExports.kt

--- src/main/kotlin/TextExports.kt
+++ src/main/kotlin/TextExports.kt
@@ -1,32 +1,38 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+import kotlin.text.substringAfter as substringAfter1
+import kotlin.text.substringAfterLast as substringAfterLast1
+import kotlin.text.substringBefore as substringBefore1
+import kotlin.text.substringBeforeLast as substringBeforeLast1
 import kotlin.text.contains as textContains
 import kotlin.text.replace as textReplace
 
 @JsExport
-fun String.replaceAll(oldValue: String, newValue: String, ignoreCase: Boolean = false): String {
+inline fun String.replaceAll(oldValue: String, newValue: String, ignoreCase: Boolean = false): String {
     return textReplace(oldValue, newValue, ignoreCase)
 }
 
 @JsExport
-fun String.contains(other: String, ignoreCase: Boolean = false): Boolean {
+inline fun String.contains(other: String, ignoreCase: Boolean = false): Boolean {
     return textContains(other, ignoreCase)
 }
 
 @JsExport
-fun String.substringBefore(delimiter: String, missingDelimiterValue: String = this): String {
-    return substringBefore(delimiter, missingDelimiterValue)
+inline fun String.substringBefore(delimiter: String, missingDelimiterValue: String = this): String {
+    return substringBefore1(delimiter, missingDelimiterValue)
 }
 
 @JsExport
-fun String.substringBeforeLast(delimiter: String, missingDelimiterValue: String = this): String {
-    return substringBeforeLast(delimiter, missingDelimiterValue)
+inline fun String.substringBeforeLast(delimiter: String, missingDelimiterValue: String = this): String {
+    return substringBeforeLast1(delimiter, missingDelimiterValue)
 }
 
 @JsExport
-fun String.substringAfter(delimiter: String, missingDelimiterValue: String = this): String {
-    return substringAfter(delimiter, missingDelimiterValue)
+inline fun String.substringAfter(delimiter: String, missingDelimiterValue: String = this): String {
+    return substringAfter1(delimiter, missingDelimiterValue)
 }
 
 @JsExport
-fun String.substringAfterLast(delimiter: String, missingDelimiterValue: String = this): String {
-    return substringAfterLast(delimiter, missingDelimiterValue)
+inline fun String.substringAfterLast(delimiter: String, missingDelimiterValue: String = this): String {
+    return substringAfterLast1(delimiter, missingDelimiterValue)
 }
